84de7a8137f7acb20a4a0b18ddbe6a2295b4662b
micsoni <micsoni@gmail.com>
2020-03-06 11:58:18 +0100

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const express = require("express");
+const Sse = require("json-sse");
+const cors = require("cors");
 const app = express();
 
 port = 4000;
 
+const db = {};
+db.messages = [];
 
+const corsMiddleware = cors();
+const parser = express.json();
+
+const stream = new Sse();
+
+app.use(corsMiddleware);
+app.use(parser);
+
+app.get("/stream", (request, response) => {
+  stream.updateInit(db.messages);
+  stream.init(request, response);
+  console.log("hello");
+});
+
+app.post("/message", (request, response) => {
+  const { text } = request.body;
+
+  db.messages.push(text);
+
+  response.send(text);
+
+  stream.send(text);
+  console.log(db);
+});
 
 app.listen(port, () => console.log(`Listening on ${port}`));
